b9fe2cb09cb043040d2749034e85040df95bcb27
asetiawan9 <zep.setiawan09@gmail.com>
2022-09-30 13:56:05 +0700

    add validation edit

--- src/routers/contact.js
+++ src/routers/contact.js
@@ -8,12 +8,20 @@ const contactValidation = [
     .exists({checkFalsy: true}).withMessage('Enter an Email')
     .isEmail().withMessage('Wrong Email Format'),
 ];
+const contactEditValidation = [
+  body('email')
+    .exists({checkFalsy: true}).withMessage('Enter an Email')
+    .isEmail().withMessage('Wrong Email Format'),
+    body('name')
+    .exists({checkFalsy: true}).withMessage('Enter an Username')
+    .isLength({min: 6}).withMessage('Username must be more than 6 characters'),
+];
 
 contact.post('/', ...contactValidation, validationCheck, contactController.postContact);
 contact.get('/get-data', body('limit').toInt(), body('page').toInt(), contactController.getContact);
 contact.get('/detail-data/:id', contactController.detailData);
 contact.delete('/delete/:id', contactController.deleteData);
-contact.patch('/edit/:id', contactController.editData);
+contact.patch('/edit/:id', ...contactEditValidation, validationCheck, contactController.editData);
 
 module.exports = contact ;
 
